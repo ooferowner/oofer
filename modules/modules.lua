17c2ce5c5966e6a85a2ed0d4951e932829402cd8
ooferowner <hamzanoor302013@gmail.com>
2025-08-24 15:09:56 +0100

    Update modules.lua

--- modules/modules.lua
+++ modules/modules.lua
@@ -1,34 +1,47 @@
--- ESP Module
+-- KillAura Module
 run(function()
-    local ESPModule
-    local ThicknessSlider
+    local KillAuraModule
+    local RangeSlider
+    local CooldownSlider
 
-    ESPModule = oofer.Categories.Render:CreateModule({
-        Name = 'ESP',
-        Tooltip = 'Highlights all players through walls',
+    KillAuraModule = oofer.Categories.Combat:CreateModule({
+        Name = 'KillAura',
+        Tooltip = 'Automatically attacks nearby players',
         ExtraText = function()
-            return "Thickness: " .. ThicknessSlider.Value
+            return "Range: " .. RangeSlider.Value .. " | CD: " .. CooldownSlider.Value
         end,
         Function = function(enabled)
             if enabled then
-                print("ESP Enabled | Thickness:", ThicknessSlider.Value)
-                -- ESP logic here
+                print("KillAura Enabled | Range:", RangeSlider.Value, "| Cooldown:", CooldownSlider.Value)
+                -- Start KillAura logic here
             else
-                print("ESP Disabled")
-                -- Disable ESP logic here
+                print("KillAura Disabled")
+                -- Stop KillAura logic here
             end
         end
     })
 
-    ThicknessSlider = ESPModule:CreateSlider({
-        Name = 'Outline Thickness',
-        Min = 1,
-        Max = 5,
-        Default = 2,
-        Suffix = function(val) return "px" end,
+    RangeSlider = KillAuraModule:CreateSlider({
+        Name = 'Attack Range',
+        Min = 5,
+        Max = 20,
+        Default = 15,
+        Suffix = function(val) return " studs" end,
         Function = function(val)
-            print("ESP Thickness set to", val)
-            -- Update ESP thickness logic here
+            print("KillAura range set to", val)
+            -- Update range logic here
+        end
+    })
+
+    CooldownSlider = KillAuraModule:CreateSlider({
+        Name = 'Cooldown',
+        Min = 0.05,
+        Max = 1,
+        Default = 0.2,
+        Suffix = function(val) return "s" end,
+        Function = function(val)
+            print("KillAura cooldown set to", val)
+            -- Update cooldown logic here
         end
     })
 end)
